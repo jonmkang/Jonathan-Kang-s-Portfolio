acb3b3ca48782ed8fd33fea558590d217a8ce9a9
Jonathan Kang <jon.m.kang@gmail.com>
2020-09-27 10:55:09 -0400

    Updates

---
+++ precache-manifest.b4275e45f40b810c037e29d1baee8ed2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2950f6b57b372d05e3061b936f560915",
+    "url": "/Jonathan-Kang-s-Portfolio/index.html"
+  },
+  {
+    "revision": "baa82f53943171a62488",
+    "url": "/Jonathan-Kang-s-Portfolio/static/css/main.ad46cae4.chunk.css"
+  },
+  {
+    "revision": "5e4977b5de94e74de930",
+    "url": "/Jonathan-Kang-s-Portfolio/static/js/2.85b6f2a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jonathan-Kang-s-Portfolio/static/js/2.85b6f2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa82f53943171a62488",
+    "url": "/Jonathan-Kang-s-Portfolio/static/js/main.818649a0.chunk.js"
+  },
+  {
+    "revision": "ad30579e700482e5d820",
+    "url": "/Jonathan-Kang-s-Portfolio/static/js/runtime-main.aeb0ded6.js"
+  },
+  {
+    "revision": "ca350c37250209d7289ca5842a413492",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/budget-me-pic1.ca350c37.png"
+  },
+  {
+    "revision": "de7579138c91c1eb84bae252c18d92b1",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/budget-me-pic2.de757913.png"
+  },
+  {
+    "revision": "69d4ec042b3008e65671d6d5a4a7e658",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/budget-me-pic3.69d4ec04.png"
+  },
+  {
+    "revision": "c7aa2bdd293660d60079d7ce892c40fb",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/budget-me-pic4.c7aa2bdd.png"
+  },
+  {
+    "revision": "4fa3d9a0c81fcaa3f66872a1796db035",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/intro-api-p1.4fa3d9a0.jpg"
+  },
+  {
+    "revision": "b4c47c1f376bf2cbcebd6a985134f59a",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/intro-api-p2.b4c47c1f.jpg"
+  },
+  {
+    "revision": "328ee005644b9b122d84050e8ab1a39d",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/intro-api-p3.328ee005.jpg"
+  },
+  {
+    "revision": "26cbe66fa76e613ce238b8c4f2f50e5f",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/photo-of-me.26cbe66f.jpg"
+  },
+  {
+    "revision": "30cf8863eec1493daee60ee4bb9718a4",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/tabletop-bartender-p1.30cf8863.png"
+  },
+  {
+    "revision": "5e0add6ead21a1aaa83bd874e49ae37f",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/tabletop-bartender-p2.5e0add6e.jpg"
+  },
+  {
+    "revision": "5121fec74712375c4b4d6a596f78557b",
+    "url": "/Jonathan-Kang-s-Portfolio/static/media/tabletop-bartender-p3.5121fec7.jpg"
+  }
+]);
